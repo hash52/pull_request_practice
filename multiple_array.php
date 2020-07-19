5e6e1fe2b39d17e45ac563a1e2794b879dcf9631
Shoma700 <ec2-user@ip-172-31-27-225.us-east-2.compute.internal>
2020-07-19 10:28:22 +0000

    修正

--- multiple_array.php
+++ multiple_array.php
@@ -1,10 +1,12 @@
-<?
-echo multiple_array([2,4,6,7,8,8]);
+<?php
+echo multiple_array([2,4,2,4]);
+
 
 function multiple_array($array){
-  $result = 0;
+  $result = 1;
   foreach ($array as $num) {
     $result *= $num;
   }
   return $result;
 }
+
